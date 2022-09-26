50c300db5a0c3e551ed55595ef5691b631bf80e2
Aristocrat <114379997+YellowLuciteGhost@users.noreply.github.com>
2022-09-26 13:13:49 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+
+file_name = " "
+x = 0
+
+t=0
+while t <=1:
+     f = open(f"{file_name}%s.txt" % x, 'w+')
+     x += 1
+     for i in range(1):
+          f.write("aristocrat doesn't belong")
+
+
+
+
+
